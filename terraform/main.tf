f74e64ef4eb52cad6528583b85e42952952620f4
Ta'Nara N. Taylor <tanara.taylor@yahoo.com>
2026-03-09 17:03:17 -0400

    Use Terraform variables for region and instance type

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 # Terraform configuration for deploying a Linux server
 
 provider "aws" {
-  region = "us-east-1"
+ region = var.aws_region
 }
 
 resource "aws_instance" "cloud_linux_server" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "cloud-linux-automation-lab"
